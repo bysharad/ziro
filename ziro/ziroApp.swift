ba8c242836b3b63682201749b33b8cd73dd07f97
HiddenCedar <hiddencedar@proton.me>
2026-06-27 20:15:08 +0530

    Initial Commit

---
+++ ziro/ziroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ziroApp.swift
+//  ziro
+//
+//  Created by Sharad Vishwakarma on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ziroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
